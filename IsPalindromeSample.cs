63c8cc34bcf77545158b4e7d793416d450e3a4be
Dinesh Wagh <53896730+dinundmn@users.noreply.github.com>
2021-04-19 21:29:21 +0530

    Add files via upload
    Palindrome in c#

---
+++ IsPalindromeSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class IsPalindrome
+    {
+        private static bool IsPalindrome(string input)
+        {
+            IEnumerable<char> chaList = input.Reverse<char>();
+
+            string result = new string(chaList.ToArray());
+            return (string.Compare(input, result, true) == 0);
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "MADAM";
+            if (IsPalindrome(input))
+                Console.WriteLine("string is polindrome");
+            else
+                Console.WriteLine("string is not polindrome");
+
+            Console.ReadLine();
+        }
+    }
+}
